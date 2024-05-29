54bb40da73037f9066466eea60f61b052671df04
Eunike <eunike@Mac-mini.local>
2024-05-29 12:08:24 +0800

    add 5 boxes

--- sketches/rect_5.js
+++ sketches/rect_5.js
@@ -1,14 +1,13 @@
 let puzzle;
 
 function setup() {
-  createCanvas(1200, 900); // Increase the canvas size
+  createCanvas(1200, 500); // Increase the canvas size
   let smallRects = [
     new Rectangle(50, 50, 100, 100, color(255, 0, 0)),
     new Rectangle(200, 50, 100, 100, color(0, 255, 0)),
     new Rectangle(350, 50, 100, 100, color(0, 0, 255)),
     new Rectangle(500, 50, 100, 100, color(255, 255, 0)),
     new Rectangle(650, 50, 100, 100, color(0, 255, 255)),
-    new Rectangle(50, 200, 100, 100, color(255, 0, 255)),
   ];
 
   let guidelineColors = [
@@ -17,10 +16,9 @@ function setup() {
     color(0, 0, 255, 100),
     color(255, 255, 0, 100),
     color(0, 255, 255, 100),
-    color(255, 0, 255, 100),
   ];
 
-  puzzle = new Puzzle(200, 320, 800, 480, smallRects, guidelineColors, 2, 3);
+  puzzle = new Puzzle(50, 220, 800, 150, smallRects, guidelineColors, 1, 5);
 }
 
 function draw() {
